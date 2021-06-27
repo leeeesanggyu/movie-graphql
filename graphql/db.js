0e10d1d8af36424b7b557bdd539f51e0f4ab86b5
skkkk98 <salgu1998@gmail.com>
2021-06-28 00:57:52 +0900

    fetch 사용

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,14 @@
-export let movies = [
-    {
-        id: 1,
-        name: '어바웃타임',
-        score: 100,
-    },
-    {
-        id: 2,
-        name: '업',
-        score: 200,
-    },
-    {
-        id: 3,
-        name: '코코',
-        score: 90,
-    },
-    {
-        id: 4,
-        name: '하트시그널',
-        score: 1000,
-    },
-]
+import fetch from "node-fetch";
 
-export const getMoives = () => movies
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"
 
-export const getById = id => {
-    const foundMovie = movies.find(movie => id === movie.id)
-    return foundMovie
-}
+export const getMoives = (limit, rating) => {
+    let REQEUST_URL = API_URL;
 
-export const deleteById = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id)
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true
-    } else {
-        return false
-    }
-}
+    if(limit > 0) REQEUST_URL += `limit=${limit}`;
+    if(rating > 0) REQEUST_URL += `&minimum_rating=${rating}`;
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    }
-    movies.push(newMovie)
-    return newMovie
+    return fetch(REQEUST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies)
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getMoives, getById, addMovie, deleteById } from './db'
+import { getMoives } from './db'
 
 const resolvers = {
     Query: {
-        movies: (_, {}) => getMoives(),
-        movie: (_, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_, { name, score }) => addMovie(name, score),
-        deleteMovie: (_, { id }) => deleteById(id)
+        movies: (_, { limit, rating }) => getMoives( limit, rating ),
     }
 }
 
